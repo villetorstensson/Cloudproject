0c05b69d9c381da23d6cf18f6ffb64e7d00e4683
xilef88 <37808696+xilef88@users.noreply.github.com>
2020-05-28 12:21:19 +0200

    login progress
    this sucks

--- index.js
+++ index.js
@@ -19,6 +19,13 @@ app.use((req, res, next) => {
     console.info(`Got request on ${req.path} (${req.method}).`);
     next();
 });
+// Firebase App (the core Firebase SDK) is always required and
+// must be listed before other Firebase SDKs
+var firebase = require("firebase/app");
+
+// Add the Firebase products that you want to use
+require("firebase/auth");
+require("firebase/firestore");
 
 // Add a route for the path /
 app.get("/", (req, res) => {
